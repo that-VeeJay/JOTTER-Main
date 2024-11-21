a05908b9eaf48be4156c6950bb489baf30a9c5b7
Vee Jay <veejaymejosomisol@gmail.com>
2024-11-21 22:18:22 +0800

    Implemented lazy loading for main components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { lazy, Suspense, useContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppContext } from "./Context/AppContext";
+import Layout from "./Pages/Layout";
 
-const Layout = lazy(() => import("./Pages/Layout"));
 const Creators = lazy(() => import("./Pages/Creators"));
 const Dashboard = lazy(() => import("./Pages/Dashboard"));
 const Community = lazy(() => import("./Pages/Community"));
@@ -11,24 +11,31 @@ const Register = lazy(() => import("./Pages/Auth/Register"));
 
 const Loading = () => <div className="loading-spinner">Loading...</div>;
 
+const withSuspense = (Component) => {
+    return (
+        <Suspense fallback={<Loading />}>
+            <Component />
+        </Suspense>
+    );
+};
+
 export default function App() {
     const { theme } = useContext(AppContext);
 
     return (
         <main className={`${theme} text-foreground bg-background`}>
             <BrowserRouter>
-                <Suspense fallback={<Loading />}>
-                    <Routes>
-                        <Route path="/" element={<Layout />}>
-                            <Route index element={<Dashboard />} />
-                            <Route path="creators" element={<Creators />} />
-                            <Route path="community" element={<Community />} />
-                        </Route>
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/login" element={<Login />} />
-                    </Routes>
-                </Suspense>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={withSuspense(Dashboard)} />
+                        <Route path="creators" element={withSuspense(Creators)} />
+                        <Route path="community" element={withSuspense(Community)} />
+                    </Route>
+                    <Route path="/register" element={withSuspense(Register)} />
+                    <Route path="/login" element={withSuspense(Login)} />
+                </Routes>
             </BrowserRouter>
         </main>
     );
 }
+
